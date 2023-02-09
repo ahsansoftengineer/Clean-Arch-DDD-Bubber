cfeac177af694cff2fec68d297018047b9f27f8b
Muhammad Ahsan <53137713+ahsansoftengineer@users.noreply.github.com>
2023-02-09 09:26:27 +0500

    MAPSTER : PART 1 COMPLETED

---
+++ Donation.PlayGround/MyTypes.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Donation.PlayGround
+{
+  public record User(
+    int Id,
+    string FirstName,
+    string LastName
+  );
+
+  public record UserResponse(
+    int Id,
+    string FirstName,
+    string LastName,
+    string FullName,
+    Guid TraceId
+  ) : IValidatable;
+
+  public interface IValidatable
+  {
+    void Validate()
+    {
+      Console.WriteLine("Validating...");
+    }
+  }
+}
