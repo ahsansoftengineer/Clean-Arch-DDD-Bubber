f163b37953e7c7b1e0295cb9910d0b8f2f44c2d0
Muhammad Ahsan <53137713+ahsansoftengineer@users.noreply.github.com>
2023-02-20 09:28:28 +0500

    HostId issue Remains

--- Donation.Domain/HostAggregate/ValueObjects/HostId.cs
+++ Donation.Domain/HostAggregate/ValueObjects/HostId.cs
@@ -5,14 +5,17 @@ namespace Donation.Domain.Host.ValueObjects
   public sealed class HostId : ValueObject
   {
     public Guid Value { get; }
-
     private HostId(Guid value)
     {
       Value = value;
     }
     public static HostId CreateUnique()
     {
-      return new(Guid.NewGuid());
+      return new HostId(Guid.NewGuid());
+    }
+    public static HostId Create(Guid value)
+    {
+      return new HostId(value);
     }
     public override IEnumerable<object> GetEqualityComponents()
     {
