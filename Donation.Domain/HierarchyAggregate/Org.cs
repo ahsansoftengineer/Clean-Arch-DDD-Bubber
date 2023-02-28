66a71c7780e3a060fd88ffd5bbac44473cf286ce
Muhammad Ahsan <53137713+ahsansoftengineer@users.noreply.github.com>
2023-02-28 08:50:42 +0500

    Working...

--- Donation.Domain/HierarchyAggregate/Org.cs
+++ Donation.Domain/HierarchyAggregate/Org.cs
@@ -1,35 +1,11 @@
 ﻿using Donation.Domain.Common.Models;
-using Donation.Domain.Menu.Entities;
 
 namespace Donation.Domain.Hierarchy
 {
   public sealed class Org : AggregateRootBase
   {
-    private readonly List<Systemz> _systemz = new();
-
-    public IReadOnlyList<Systemz> Systemzs => _systemz.AsReadOnly();
-
-    private Org(
-        GenericValueObjectId id,
-        string title,
-        string description,
-        List<Systemz>? systems)
-        : base(id, title, description)
-    {
-      _systemz = systems;
-    }
-
-    public static Org Create(
-      string title,
-      string description,
-      List<Systemz>? system)
-    {
-      return new(
-          GenericValueObjectId.CreateUnique(),
-          title,
-          description,
-          system);
-    }
+    private readonly List<GenericValueObjectId> systemzIds = new();
+    public IReadOnlyList<GenericValueObjectId> SystemzIds => systemzIds.AsReadOnly();
 
     // Private Constructor is Required for EF Core
 #pragma warning disable CS8618
