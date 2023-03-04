62e4a188e62fd44ac8d859ee5070b5c4fb447f10
Muhammad Ahsan <53137713+ahsansoftengineer@users.noreply.github.com>
2023-03-04 16:52:35 +0500

    HIERARCHY COMMAND HANDLER CREATED

---
+++ Donation.Application/Hierarchy/Commands/CreateHierarchyCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using Donation.Application.Common.Commands;
+using Donation.Application.Common.Persistence;
+using Donation.Domain.Hierarchy;
+
+namespace Donation.Application.Hierarchy.Commands
+{
+  public class CreateOrgCommandHandler : CreateSimpleCommandHandler<IOrgRepo, Org>
+  {
+    public CreateOrgCommandHandler(IOrgRepo repo) : base(repo)
+    {
+    }
+  }
+  public class CreateSimpleChildCommandHandler : CreateSimpleChildCommandHandler<ISystemzRepo, Systemz>
+  {
+    public CreateSimpleChildCommandHandler(ISystemzRepo repo) : base(repo)
+    {
+    }
+  }
+  public class CreateBGCommandHandler : CreateSimpleCommandHandler<IBGRepo, BG>
+  {
+    public CreateBGCommandHandler(IBGRepo repo) : base(repo)
+    {
+    }
+  }
+  public class CreateLECommandHandler : CreateSimpleChildCommandHandler<ILERepo, LE>
+  {
+    public CreateLECommandHandler(ILERepo repo) : base(repo)
+    {
+    }
+  }
+  public class CreateOUCommandHandler : CreateSimpleChildCommandHandler<IOURepo, OU>
+  {
+    public CreateOUCommandHandler(IOURepo repo) : base(repo)
+    {
+    }
+  }
+  public class CreateSUCommandHandler : CreateSimpleChildCommandHandler<ISURepo, SU>
+  {
+    public CreateSUCommandHandler(ISURepo repo) : base(repo)
+    {
+    }
+  }
+}
--- Donation.Infrastructure/Persistence/DonationDbContext.cs
+++ Donation.Infrastructure/Persistence/DonationDbContext.cs
@@ -1,7 +1,6 @@
 ﻿using Donation.Domain.Hierarchy;
 using Donation.Domain.Menu;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Donation.Infrastructure.Persistence
 {
@@ -12,26 +11,18 @@ namespace Donation.Infrastructure.Persistence
 
     }
     public DbSet<Menu> Menus { get; set; } = null!;
-    //public DbSet<Org> Orgs { get; set; } = null!;
-    //public DbSet<Systemz> Systemzs { get; set; } = null!;
-    //public DbSet<BG> BGs { get; set; } = null!;
-    //public DbSet<LE> LEs { get; set; } = null!;
-    //public DbSet<OU> OUs { get; set; } = null!;
-    //public DbSet<SU> SUs { get; set; } = null!;
+    public DbSet<Org> Orgs { get; set; } = null!;
+    public DbSet<Systemz> Systemzs { get; set; } = null!;
+    public DbSet<BG> BGs { get; set; } = null!;
+    public DbSet<LE> LEs { get; set; } = null!;
+    public DbSet<OU> OUs { get; set; } = null!;
+    public DbSet<SU> SUs { get; set; } = null!;
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
       modelBuilder.ApplyConfigurationsFromAssembly(
         typeof(DonationDbContext).Assembly
       );
-
-      //modelBuilder.Model.GetEntityTypes()
-      //  .SelectMany(e => e.GetProperties())
-      //  .Where(p => p.IsPrimaryKey())
-      //  .ToList()
-      //  .ForEach(p => p.ValueGenerated = ValueGenerated.Never);
-
       base.OnModelCreating(modelBuilder);
     }
   }
 }
-// dotnet add Donation.Infrastructure package Microsoft.EntityFrameworkCore.SqlServer
