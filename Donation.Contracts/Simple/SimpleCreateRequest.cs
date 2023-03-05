eed57addf04619d079ae72b773d5a0d6e001a085
Muhammad Ahsan <53137713+ahsansoftengineer@users.noreply.github.com>
2023-03-05 16:47:42 +0500

    7-Create-Request-Response

---
+++ Donation.Contracts/Simple/SimpleCreateRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Donation.Contracts.Simple
+{
+  public record SimpleCreateRequest(
+    string Title,
+    string Description);
+
+  public record SimpleChildCreateRequest(
+    string Title,
+    string Description,
+    string ParentId);
+}
---
+++ Donation.Contracts/Simple/SimpleCreateResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace Donation.Contracts.Simple
+{
+  public record SimpleOption(
+    string Title,
+    string Description);
+  public record SimpleCreateResponse(
+    string Id,
+    string Title,
+    string Description);
+
+  public record SimpleChildCreateResponse(
+    string Id,
+    string Title,
+    string Description,
+    string ParentId,
+    SimpleOption Parent);
+
+}
