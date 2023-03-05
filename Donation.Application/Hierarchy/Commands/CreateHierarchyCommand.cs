0ba079ee8443da9276666bfd4c6e6b34c75c6f8e
Muhammad Ahsan <53137713+ahsansoftengineer@users.noreply.github.com>
2023-03-05 16:49:48 +0500

    8-Commands-&-Validator

---
+++ Donation.Application/Hierarchy/Commands/CreateHierarchyCommand.cs
@@ -0,0 +1,37 @@
+﻿
+using Donation.Domain.HierarchyAggregate;
+using ErrorOr;
+using MediatR;
+
+namespace Donation.Application.Hierarchy.Commands
+{
+  public record CreateOrgCommand(
+    string Title,
+    string Description) : IRequest<ErrorOr<Org>>;
+
+  public record CreateSystemzCommand(
+    string Title,
+    string Description,
+    string ParentId
+    ) : IRequest<ErrorOr<Systemz>>;
+
+  public record CreateBGCommand(
+    string Title,
+    string Description) : IRequest<ErrorOr<BG>>;
+
+  public record CreateLECommand(
+    string Title,
+    string Description,
+    string ParentId) : IRequest<ErrorOr<LE>>;
+
+  public record CreateOUCommand(
+    string Title,
+    string Description,
+    string ParentId) : IRequest<ErrorOr<OU>>;
+
+  public record CreateSUCommand(
+    string Title,
+    string Description,
+    string ParentId) : IRequest<ErrorOr<SU>>;
+
+}
---
+++ Donation.Application/Hierarchy/Commands/CreateHierarchyCommandValidator.cs
@@ -0,0 +1,59 @@
+﻿using FluentValidation;
+
+namespace Donation.Application.Hierarchy.Commands
+{
+  public class CreateOrgCommandValidator : AbstractValidator<CreateOrgCommand>
+  {
+    public CreateOrgCommandValidator() {
+      RuleFor(x => x.Title).NotEmpty();
+      RuleFor(x => x.Description).NotEmpty();
+    }
+  }
+
+  public class CreateSystemzCommandValidator : AbstractValidator<CreateSystemzCommand>
+  {
+    public CreateSystemzCommandValidator()
+    {
+      RuleFor(x => x.Title).NotEmpty();
+      RuleFor(x => x.Description).NotEmpty();
+      RuleFor(x => x.ParentId).NotEmpty();
+    }
+  }
+
+  public class CreateBGCommandValidator : AbstractValidator<CreateBGCommand>
+  {
+    public CreateBGCommandValidator()
+    {
+      RuleFor(x => x.Title).NotEmpty();
+      RuleFor(x => x.Description).NotEmpty();
+    }
+  }
+
+  public class CreateLECommandValidator : AbstractValidator<CreateLECommand>
+  {
+    public CreateLECommandValidator()
+    {
+      RuleFor(x => x.Title).NotEmpty();
+      RuleFor(x => x.Description).NotEmpty();
+      RuleFor(x => x.ParentId).NotEmpty();
+    }
+  }
+  public class CreateOUCommandValidator : AbstractValidator<CreateOUCommand>
+  {
+    public CreateOUCommandValidator()
+    {
+      RuleFor(x => x.Title).NotEmpty();
+      RuleFor(x => x.Description).NotEmpty();
+      RuleFor(x => x.ParentId).NotEmpty();
+    }
+  }
+  public class CreateSUCommandValidator : AbstractValidator<CreateSUCommand>
+  {
+    public CreateSUCommandValidator()
+    {
+      RuleFor(x => x.Title).NotEmpty();
+      RuleFor(x => x.Description).NotEmpty();
+      RuleFor(x => x.ParentId).NotEmpty();
+    }
+  }
+}
