553ec90b49a64518e811ee42a5a7e6125aefaf5d
Muhammad Ahsan <53137713+ahsansoftengineer@users.noreply.github.com>
2023-03-05 16:52:26 +0500

    A1-Mapping-Config

---
+++ Donation.Api/Common/Mapping/Hierarchy/OrgMappingConfig.cs
@@ -0,0 +1,23 @@
+﻿using Donation.Application.Hierarchy.Commands;
+using Donation.Contracts.Simple;
+using Donation.Domain.HierarchyAggregate;
+using Mapster;
+// OrgAggregate
+
+namespace Donation.Api.Common.Mapping.Hierarchy
+{
+  public class OrgMappingConfig : IRegister
+  {
+    public void Register(TypeAdapterConfig config)
+    {
+      config.NewConfig<
+        (SimpleCreateRequest Request, string HostId),  // src area
+        CreateOrgCommand>() // dest area
+        .Map(dest => dest, src => src.Request);
+
+      config.NewConfig<Org, SimpleCreateResponse>()
+        .Map(dest => dest.Id, src => src.Id.Value);
+
+    }
+  }
+}
---
+++ Donation.Api/Common/Mapping/Hierarchy/SystemzMappingConfig.cs
@@ -0,0 +1,27 @@
+﻿using Donation.Application.Hierarchy.Commands;
+using Donation.Contracts.Simple;
+using Donation.Domain.HierarchyAggregate;
+using Mapster;
+// SystemzAggregate
+
+namespace Donation.Api.Common.Mapping.Hierarchy
+{
+  public class SystemzMappingConfig : IRegister
+  {
+    public void Register(TypeAdapterConfig config)
+    {
+      config.NewConfig<
+        (SimpleChildCreateRequest Request, string HostId),  // src area
+        CreateSystemzCommand>() // dest area
+        .Map(dest => dest, src => src.Request)
+        .Map(dest => dest, src => src.Request);
+
+      config.NewConfig<Systemz, SimpleChildCreateResponse>()
+        .Map(dest => dest.Id, src => src.Id.Value)
+        .Map(dest => dest.ParentId, src => src.OrgId.Value);
+        //.Map(dest => dest.Parent, src => src.Org.Select(org => org.Value))
+        //.Map(dest => dest.ParentId, src => src.Org); // How this is going to done
+
+    }
+  }
+}
